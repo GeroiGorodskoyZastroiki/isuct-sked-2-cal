5bf433609811cd204fa4f8b45223eefc8af11af5
GeroiGorodskoyZastroiki <denhardx@gmail.com>
2023-02-04 01:35:16 +0300

    path fix

--- client/src/components/SubjectsNicks.vue
+++ client/src/components/SubjectsNicks.vue
@@ -16,8 +16,8 @@ function getSubjects() {
 
 function setNick(event) {
   console.log(event)
-  let actualName = event.path[1].children[0].textContent
-  let nick = event.path[1].children[1].value
+  let actualName = event.target.parentElement.children[0].textContent
+  let nick = event.target.parentElement.children[1].value
   if (actualName == nick) return
   nicks[actualName] = nick
 }
